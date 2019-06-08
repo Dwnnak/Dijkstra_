cbf47b485751b8736c43cee64eda83b97c57748c
DRed <dima.redkin.1999@gmail.com>
2019-06-08 20:53:44 +0500

    comment GraphExtensions class

--- DijkstraAlgorhitm/GraphExtensions.cs
+++ DijkstraAlgorhitm/GraphExtensions.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 namespace DijkstraAlgorhitm
 {
+    /// <summary>
+    /// Extension methods for Graph class
+    /// </summary>
     public static class GraphExtensions
     {
+        /// <summary>
+        /// simplification: call Dijkstra algorithm from instance of Graph
+        /// </summary>
+        /// <param source graph ="graph"></param>
+        /// <param source node="source"></param>
+        /// <returns></returns>
         public static Graph FindDijkstraShortestPathsFrom(this Graph graph, DijkstraNode source)
         {
             var dijkstra = new DijkstraAlgorhitm();
