94e39e6a51f6a6bb5f8b6e7dbb7f93e4a89e3345
DRed <dima.redkin.1999@gmail.com>
2019-06-08 21:38:34 +0500

    Graph class comments

--- DijkstraAlgorhitm/AdjacencyDictionary.cs
+++ DijkstraAlgorhitm/AdjacencyDictionary.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 namespace DijkstraAlgorhitm
 {
     /// <summary>
-    /// mathes node to adjacent nodes with edges weights to them.
-    /// For example from node U to V1, V2, ... Vn 
-    /// with edges weights (from U to V1, V2, ... Vn)
+    /// mathes every node to adjacent nodes with edges weights to them.
+    /// For example from node U to V1, V2, ..., Vn 
+    /// with edges weights (from U to V1, V2, ..., Vn)
     /// </summary>
     public class AdjacencyDictionary
     {
+        /// <summary>
+        /// keys is nodes of graph it mathes to
+        /// values which are lists of all adjacent nodes with weights to them
+        /// </summary>
         private Dictionary<DijkstraNode, List<(DijkstraNode node, int weight)>> AdjDictionary
         { get; set; }
 
@@ -26,11 +30,20 @@ namespace DijkstraAlgorhitm
             AdjDictionary[source].Add((destination, weight));
         }
 
+        /// <summary>
+        /// Get all the keys that are the vertices of the graph
+        /// </summary>
+        /// <returns></returns>
         public Dictionary<DijkstraNode, List<(DijkstraNode, int)>>.KeyCollection GetVertices()
         {
             return AdjDictionary.Keys;
         }
 
+        /// <summary>
+        /// Get List of adjacent nodes with edges weights to them
+        /// </summary>
+        /// <param the node the list of which we want to get ="node"></param>
+        /// <returns></returns>
         public List<(DijkstraNode node, int weight)> this[DijkstraNode node]
         {
             get
--- DijkstraAlgorhitm/Graph.cs
+++ DijkstraAlgorhitm/Graph.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 namespace DijkstraAlgorhitm
 {
+    /// <summary>
+    /// implements a graph structure as AdjacencyDictionary
+    /// </summary>
     public class Graph
     {
+        /// <summary>
+        /// stores nodes of graph and adjacent nodes 
+        /// with edges weights to them
+        /// </summary>
         public AdjacencyDictionary AdjDict { get; set; }
 
+        /// <summary>
+        /// add vertice to graph
+        /// </summary>
+        /// <param name of future node="name"></param>
+        /// <returns></returns>
         public DijkstraNode AddNode (string name)
         {
             return AdjDict.AddNode(name);
         }
 
+        /// <summary>
+        /// add edge to graph
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <param name="weight"></param>
         public void AddEdge(DijkstraNode u, DijkstraNode v, int weight)
         {
             AdjDict.AddEdge(u, v, weight);
