93972c1354563131bc61ab307fa725141dc474a7
DRed <dima.redkin.1999@gmail.com>
2019-06-09 14:22:12 +0500

    Dijkstra tests done

--- DijkstraAlgorhitm/DictionaryExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace DijkstraAlgorhitm
-{
-    public class DictionaryExtensions
-    {
-        // return first with ...
-    }
-}
--- Tests/DijkstraTests.cs
+++ Tests/DijkstraTests.cs
@@ -30,13 +30,10 @@ namespace Tests
             g.AddEdge(a, b, 1);
             g.AddEdge(b, a, 2);
             g.AddEdge(b, c, 4);
-            g.FindDijkstraShortestPathsFrom(a);
-            var nodes = g.AdjDict.GetVertices();
-            var distances = nodes.Select(node => $"to {node.Name}= {node.Distance}");
-            string result = string.Join(", ", distances);
-            var paths = $"source is a. {result}";
-            Assert.AreEqual("source is a. to a= 0, to b= 1, to c= 5", paths);
 
+            g.FindDijkstraShortestPathsFrom(a);
+            var paths = DijkstraResultFormating(g, a);
+            Assert.AreEqual("source is a. to a= 0, to b= 1, to c= 5", paths);
         }
 
         /*   
@@ -93,13 +90,14 @@ namespace Tests
             g.AddEdge(c, e, 2);
             g.AddEdge(d, e, 7);
             g.AddEdge(e, d, 9);
+
             g.FindDijkstraShortestPathsFrom(a);
             string paths = DijkstraResultFormating(g, a);
             Assert.AreEqual
                 ("source is a. to a= 0, to b= 7, to c= 3, to d= 9, to e= 5", 
                 paths);
         }
-
+        
         private static string DijkstraResultFormating(Graph g, DijkstraNode source)
         {
             var nodes = g.AdjDict.GetVertices();
